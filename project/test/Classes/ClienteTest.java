5e351d668ed39bc39dc2e0c577451e9eb1abe882
davikawasaki <davishinjik@gmail.com>
2016-10-04 01:21:59 -0300

    Testes caixa preta de cliente + Manutenção classe cliente

--- project/test/Classes/ClienteTest.java
+++ project/test/Classes/ClienteTest.java
@@ -14,25 +14,85 @@ import static org.junit.Assert.*;
  */
 public class ClienteTest {
     @Test
-    public void ContatoInsereIdCliente() {
+    public void testeIdClienteValido() throws Exception {
         Cliente cliente = new Cliente();
         cliente.setIdCliente(55555);
         int valorRecebido = cliente.getIdCliente();
         assertEquals(valorRecebido, 55555);
-    }  
+    }
+    // ID inválido para ID < 0
     @Test
-    public void ContatoInsereFidelidade() {
+    public void testeIdClienteInvalido() throws Exception {
+        Cliente cliente = new Cliente();
+        String expResult = "ID Invalido";
+        try {
+            cliente.setIdCliente(-1);
+            int valorRecebido = cliente.getIdCliente();
+            fail("Deveria ter lançado uma exceção!");
+        } catch(Exception e) {
+            assertEquals(expResult, e.getMessage());
+        }
+    } 
+    @Test
+    public void testeFidelidadeClienteValido() {
         Cliente cliente = new Cliente();
         cliente.setFidelidade(true);
         boolean valorRecebido = cliente.isFidelidade();
         assertEquals(valorRecebido, true);
     }  
     @Test
-    public void ContatoInsereCodFidelidade() {
+    public void testeCodFidelidadeClienteValido() throws Exception {
         Cliente cliente = new Cliente();
         cliente.setCodFidelidade("A123");
         String valorRecebido = cliente.getCodFidelidade();
         assertEquals(valorRecebido, "A123");
     }  
-
+    // Codigo de Fidelidade inválido menor que 3 caracteres
+    @Test
+    public void testeCodFidelidadeClienteInvalido1() throws Exception {
+        Cliente cliente = new Cliente();
+        String expResult = "Codigo de Fidelidade menor que 3 caracteres";
+        try {
+            cliente.setCodFidelidade("A1");
+            String valorRecebido = cliente.getCodFidelidade();
+            fail("Deveria ter lançado uma exceção!");
+        } catch(Exception e) {
+            assertEquals(expResult, e.getMessage());
+        }
+    }
+    // Codigo de Fidelidade inválido estourando o limite de caracteres
+    @Test
+    public void testeCodFidelidadeClienteInvalido2() throws Exception {
+        Cliente cliente = new Cliente();
+        String expResult = "Codigo de Fidelidade maior que 45 caracteres";
+        try {
+            cliente.setCodFidelidade("A1312515131NFJ34JH1IJEI2JO1JDFOIJFO1IJ23O1J2OJ1I231FJ1I");
+            String valorRecebido = cliente.getCodFidelidade();
+            fail("Deveria ter lançado uma exceção!");
+        } catch(Exception e) {
+            assertEquals(expResult, e.getMessage());
+        }
+    }
+    // Codigo de Fidelidade inválido vazio
+    @Test
+    public void testeCodFidelidadeClienteInvalido3() throws Exception {
+        Cliente cliente = new Cliente();
+        String expResult = "Codigo de Fidelidade Invalido";
+        try {
+            cliente.setCodFidelidade("");
+            String valorRecebido = cliente.getCodFidelidade();
+            fail("Deveria ter lançado uma exceção!");
+        } catch(Exception e) {
+            assertEquals(expResult, e.getMessage());
+        }
+    }
+    @Test
+    public void testePessoaFisicaClienteValido() {
+        Cliente cliente = new Cliente();
+        PessoaFisica pf = new PessoaFisica();
+        
+        cliente.setPf(pf);
+        PessoaFisica result = cliente.getPf();
+        assertEquals(pf, result);
+    }  
 }
